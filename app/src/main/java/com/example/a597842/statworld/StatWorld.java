0a7a9a61fea1270c7eaf21a4761a35cc2ab723b0
Guerrero <597842@student.dmschools.org>
2018-02-22 10:15:25 -0600

    Initial commit

---
+++ app/src/main/java/com/example/a597842/statworld/StatWorld.java
@@ -0,0 +1,13 @@
+package com.example.a597842.statworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat_world);
+    }
+}
